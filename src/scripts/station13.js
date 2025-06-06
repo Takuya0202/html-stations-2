70341b40a6aac0b735c7d4df24487fcefb148c21
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2025-06-06 16:51:50 +0900

    station13までクリア

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,10 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
-}
+  const text = document.getElementById('text');
+  if ( e.target.checked) {
+    text.style.backgroundColor = '#ff0000';
+  }
+  else{
+    text.style.background = 'white';
+  };
+};
